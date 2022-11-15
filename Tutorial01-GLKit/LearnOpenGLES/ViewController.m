5e887a282bdeb4122b7b0453c17b025533109741
姚七六 <176zane@gmail.com>
2022-11-15 22:42:33 +0800

    注释

--- Tutorial01-GLKit/LearnOpenGLES/ViewController.m
+++ Tutorial01-GLKit/LearnOpenGLES/ViewController.m
@@ -48,9 +48,15 @@
     
     //顶点数据缓存
     GLuint buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+    glGenBuffers(1, &buffer);//生成缓冲区对象的名称
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);//标识符绑定到GL_ARRAY_BUFFER上
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);//把顶点数据从cpu内存复制到gpu内存
+    
+    // 加载顶点数据，indx参数指定通用顶点属性索引，size参数表示顶点数组中微索引引用的顶点属性所指定的分量数量，type参数表示数据格式，normalized参数表示非浮点数据类型转换为浮点值时是否应该规范化，stride参数指定顶点索引I和I+1表示的顶点数据之间的位移，ptr参数在使用客户端顶点数组时表示保存顶点属性数据的缓冲区指针，在使用顶点缓冲区对象时，表示该缓冲区内的偏移量。
+    //glVertexAttribPointer ( 0, 3, GL_FLOAT, GL_FALSE, 0, vVertices );
+//    顶点数组可以通过glBufferData放入缓存，也可以直接通过glVertexAttribPointer最后一个参数，直接把顶点数组从CPU传送到GPU。区别：glBufferData里面的顶点缓存可以复用，glVertexAttribPointer是每次都会把顶点数组从CPU发送到GPU，影响性能
+
+
     
     glEnableVertexAttribArray(GLKVertexAttribPosition); //顶点数据缓存
     glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (GLfloat *)NULL + 0);
